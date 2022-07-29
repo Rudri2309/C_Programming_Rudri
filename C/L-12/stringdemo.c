f9fe42d8778efb0b710e805c08c90a2198629522
Rudri2309 <rudri2309@gmail.com>
2022-07-29 10:46:25 +0530

    L12

--- C/L-12/stringdemo.c
+++ C/L-12/stringdemo.c
@@ -7,4 +7,11 @@ void main()
 	char empty[10];
 	printf("String Length : %d", strlen(name));
 	printf("\nString Reverse: %s",strrev(name)); 
+	printf("\nString Concatenate: %s",strcat(name,sname));
+	strcpy(empty,name);
+	printf("\nString Copy : %s",empty);
+	printf("\nString Compare : %d",strcmp(name,sname));
+	printf("\nString UpperCase : %s",strupr(name));
+	printf("\nString LowerCase : %s",strlwr(sname));
+
 }
