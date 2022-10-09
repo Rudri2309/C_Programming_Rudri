414f599df0f897bbe2735133f8c323f6a3175f3c
Rudri2309 <rudri2309@gmail.com>
2022-10-09 16:01:30 +0530

    .

---
+++ Assignments/Program 1.1.c
@@ -0,0 +1,45 @@
+//This program is prepared by Rudri Bhatt (22TCE003)
+//Program 1.1
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	
+	
+	
+	int a=1,b=4,c=3;
+	printf("\t%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c %c ",1,4,3,1);
+	
+	printf("\n\t%c\tAny energy you fight, you are feeding it. \t\t\t%c\n",3,3);
+	printf("\t%c\t\tIf you are pushing something away, \t\t\t%c\n",1,1);
+	printf("\t%c\t\t\tYou are inviting to stay.\"by Michael Singer\t%c\n",4,4);
+	
+	printf("\t%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c ",1,4,3);
+	printf("%c %c %c %c ",1,4,3,1);
+	
+	
+	getch();
+}
